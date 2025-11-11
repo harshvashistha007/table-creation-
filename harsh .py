670999fcbc73b40cdfefda06fde91d557bd6bf0d
Harsh Vashistha <07harshvashistha@gmail.com>
2025-11-11 10:42:10 +0530

    Add files via upload

---
+++ harsh .py
@@ -0,0 +1,15 @@
+''''marks=int(input("enter your marks"))
+if marks>=90:
+          print("your grade is A+")
+elif marks>=80:
+    print("your grqade is A ")'''
+
+'''student=["Asdf","sffffffd","DEEE","gashgv"]
+for i in student:
+    print (i)'''
+
+a=5
+for i in range(1,11):
+    i=i*a
+    print(i)
+          
